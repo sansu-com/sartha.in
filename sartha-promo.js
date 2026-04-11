a1ca970f92b9906d0d07532e029f65d9f795e16b
sansu-com <sanskarnarale4@gmail.com>
2026-04-11 14:58:56 +0530

    Add promotional messages to sartha-promo.js

--- sartha-promo.js
+++ sartha-promo.js
@@ -1,1 +1,21 @@
+const promos = [
+  {
+    text: "🎓 SARTHA Counselling – Personal Mentor",
+    btn: "Apply",
+    link: "https://wa.me/91XXXXXXXXXX"
+  },
+  {
+    text: "🔥 NEET 2026 Counselling Started",
+    btn: "Join",
+    link: "https://wa.me/91XXXXXXXXXX"
+  }
+];
 
+let ad = promos[Math.floor(Math.random() * promos.length)];
+
+document.getElementById("sarthaAdContainer").innerHTML = `
+  <div class="sartha-bar">
+    <div class="sartha-text">${ad.text}</div>
+    <a href="${ad.link}" class="sartha-btn">${ad.btn}</a>
+  </div>
+`;
